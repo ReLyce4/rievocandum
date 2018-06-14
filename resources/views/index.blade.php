ba48a17245ac5194d3891120d3bf2f4cc0800f32
Relyce4 <relyce4@gmail.com>
2018-06-14 11:55:34 +0200

    Added slogan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,9 +38,6 @@
             <li class="navbar-item">
               <a class="nav-link" href="#">Login</a>
             </li>
-            <li class="navbar-item">
-              <a class="nav-link" href="#">Logout</a>
-            </li>
           </ul>
         </div>
       </div>
@@ -52,19 +49,12 @@
       <div class="container">
         <div class="row">
           <div class="col-xl-9 mx-auto">
-            <h1 class="mb-5">Build a landing page for your business or project and generate more leads!</h1>
-          </div>
-          <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-            <form>
-              <div class="form-row">
-                <div class="col-12 col-md-9 mb-2 mb-md-0">
-                  <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-                </div>
-                <div class="col-12 col-md-3">
-                  <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-                </div>
-              </div>
-            </form>
+            <h1>Learn smarter with Rievocandum</h1>
+            <p>
+              Our goal is to give you tools based on the 
+              SRS(Spaced Repetition System) in order to improve the way you learn
+            </p>
+            <a class="btn btn-primary" href="#">Find out more</a>
           </div>
         </div>
       </div>
