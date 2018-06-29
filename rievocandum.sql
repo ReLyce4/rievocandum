98b9cbe40eb6cff1a95344408e2445616104de59
Relyce4 <relyce4@gmail.com>
2018-06-29 17:10:50 +0200

    Updated database schema

--- rievocandum.sql
+++ rievocandum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Giu 24, 2018 alle 10:56
+-- Creato il: Giu 29, 2018 alle 17:10
 -- Versione del server: 5.7.17
 -- Versione PHP: 7.1.3
 
@@ -34,6 +34,55 @@ CREATE TABLE `migrations` (
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dump dei dati per la tabella `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `file_name` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `notes`
+--
+
+INSERT INTO `notes` (`id`, `file_name`, `created_at`, `updated_at`, `user_id`) VALUES
+(1, 'a', '2018-06-28 19:13:33', '2018-06-28 19:13:33', 1),
+(2, 'a', '2018-06-28 19:14:25', '2018-06-28 19:14:25', 1),
+(3, 'aaaa', '2018-06-28 19:15:30', '2018-06-28 19:15:30', 1),
+(4, 'a', '2018-06-28 19:22:24', '2018-06-28 19:22:24', 1),
+(5, 'a', '2018-06-28 19:28:12', '2018-06-28 19:28:12', 1),
+(6, 'sfd', '2018-06-28 19:42:31', '2018-06-28 19:42:31', 1),
+(7, 'b', '2018-06-28 19:44:59', '2018-06-28 19:44:59', 1),
+(8, 'v', '2018-06-28 19:46:22', '2018-06-28 19:46:22', 1),
+(9, 'vca', '2018-06-28 19:59:22', '2018-06-28 19:59:22', 1),
+(10, 'documento1', '2018-06-28 20:08:50', '2018-06-28 20:08:50', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -47,15 +96,17 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `avatar_url` varchar(255) DEFAULT 'img/user_icon.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `updated_at`, `created_at`) VALUES
-(1, 'Relyce4', 'relyce4@gmail.com', '$2y$10$uOlPTvOc26zT/1UzwPcYMOdQU.DJCiz6yhctz2bKeSeuX9IVRENKi', 'y2hN7B7KgNQeEx1IlQjzdOotpPEfvIQJcjvV8kqkusoFAb7nbZyMkJ8oRzHo', '2018-06-24 08:43:24', '2018-06-23 12:24:00');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `updated_at`, `created_at`, `avatar_url`) VALUES
+(1, 'Christian', 'relyce4@gmail.com', '$2y$10$IR0wpHJIB5mJYPKDqk2MCOAU9t9G6RLNHrWPLJQtO5canw17Y5yZq', 'rslT9Cvxtd38sjJ19DqlOv3YplUeVjNcRRwkshpkYyACt7mFvh7cHK1UbObI', '2018-06-27 13:15:09', '2018-06-23 12:24:00', 'img/user_icon.png'),
+(2, 'Giuseppe', 'giuseppe@gmail.com', '$2y$10$p1N/kCnmN8s0QDcyZE0P4eou1j8lpoeQTV134PlGz8CP83G3FfeHq', 'uM2Jp2LopZZlYNgML5RaclApZWf0Akzf0UvAaAoIsnqrmPTfNpLASAUJV1i7', '2018-06-27 13:16:36', '2018-06-27 11:15:31', 'img/user_icon.png');
 
 --
 -- Indici per le tabelle scaricate
@@ -66,6 +117,19 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `updat
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
 
 --
 -- Indici per le tabelle `users`
@@ -83,12 +147,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT per la tabella `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT per la tabella `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
