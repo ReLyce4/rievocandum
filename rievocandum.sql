c4f32948df66ca93dffe17e45b7f1c26b127ed7d
Relyce4 <relyce4@gmail.com>
2018-07-02 10:52:06 +0200

    Database schema update

--- rievocandum.sql
+++ rievocandum.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Giu 29, 2018 alle 17:10
--- Versione del server: 5.7.17
--- Versione PHP: 7.1.3
+-- Generation Time: Jul 02, 2018 at 10:51 AM
+-- Server version: 5.7.17
+-- PHP Version: 7.1.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `migrations`
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`, `created_at`, `updated_at`) VALUES
+(1, 'informatica', '2018-07-01 17:31:56', '2018-07-01 17:31:56'),
+(2, 'a', '2018-07-01 17:35:56', '2018-07-01 17:35:56'),
+(3, 'aa', '2018-07-01 17:36:19', '2018-07-01 17:36:19'),
+(4, 'afdasf', '2018-07-01 19:42:52', '2018-07-01 19:42:52'),
+(5, 'giuseppe', '2018-07-02 08:23:48', '2018-07-02 08:23:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
 --
 
 CREATE TABLE `migrations` (
@@ -35,7 +59,7 @@ CREATE TABLE `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dump dei dati per la tabella `migrations`
+-- Dumping data for table `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -44,37 +68,47 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `notes`
+-- Table structure for table `notes`
 --
 
 CREATE TABLE `notes` (
   `id` int(11) NOT NULL,
-  `file_name` text NOT NULL,
+  `file_name` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dump dei dati per la tabella `notes`
+-- Dumping data for table `notes`
 --
 
-INSERT INTO `notes` (`id`, `file_name`, `created_at`, `updated_at`, `user_id`) VALUES
-(1, 'a', '2018-06-28 19:13:33', '2018-06-28 19:13:33', 1),
-(2, 'a', '2018-06-28 19:14:25', '2018-06-28 19:14:25', 1),
-(3, 'aaaa', '2018-06-28 19:15:30', '2018-06-28 19:15:30', 1),
-(4, 'a', '2018-06-28 19:22:24', '2018-06-28 19:22:24', 1),
-(5, 'a', '2018-06-28 19:28:12', '2018-06-28 19:28:12', 1),
-(6, 'sfd', '2018-06-28 19:42:31', '2018-06-28 19:42:31', 1),
-(7, 'b', '2018-06-28 19:44:59', '2018-06-28 19:44:59', 1),
-(8, 'v', '2018-06-28 19:46:22', '2018-06-28 19:46:22', 1),
-(9, 'vca', '2018-06-28 19:59:22', '2018-06-28 19:59:22', 1),
-(10, 'documento1', '2018-06-28 20:08:50', '2018-06-28 20:08:50', 1);
+INSERT INTO `notes` (`id`, `file_name`, `created_at`, `updated_at`, `user_id`, `category_id`) VALUES
+(20, 'database', '2018-07-01 17:31:56', '2018-07-01 17:31:56', 1, 1),
+(22, 'aaaa', '2018-07-01 19:42:52', '2018-07-01 19:42:52', 1, 4),
+(23, 'aa', '2018-07-01 19:50:00', '2018-07-01 19:50:00', 1, 4),
+(24, 'aaa', '2018-07-01 19:50:25', '2018-07-01 19:50:25', 1, 4),
+(26, 'a', '2018-07-02 08:22:02', '2018-07-02 08:22:02', 1, 1),
+(27, 'a', '2018-07-02 08:22:31', '2018-07-02 08:22:31', 2, 2),
+(28, 'giuseppe', '2018-07-02 08:23:48', '2018-07-02 08:23:48', 2, 5);
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `password_resets`
+-- Table structure for table `note_tag`
+--
+
+CREATE TABLE `note_tag` (
+  `id` int(11) NOT NULL,
+  `note_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
 --
 
 CREATE TABLE `password_resets` (
@@ -86,7 +120,20 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `users`
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `tag` varchar(30) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -101,38 +148,58 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dump dei dati per la tabella `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `updated_at`, `created_at`, `avatar_url`) VALUES
-(1, 'Christian', 'relyce4@gmail.com', '$2y$10$IR0wpHJIB5mJYPKDqk2MCOAU9t9G6RLNHrWPLJQtO5canw17Y5yZq', 'rslT9Cvxtd38sjJ19DqlOv3YplUeVjNcRRwkshpkYyACt7mFvh7cHK1UbObI', '2018-06-27 13:15:09', '2018-06-23 12:24:00', 'img/user_icon.png'),
-(2, 'Giuseppe', 'giuseppe@gmail.com', '$2y$10$p1N/kCnmN8s0QDcyZE0P4eou1j8lpoeQTV134PlGz8CP83G3FfeHq', 'uM2Jp2LopZZlYNgML5RaclApZWf0Akzf0UvAaAoIsnqrmPTfNpLASAUJV1i7', '2018-06-27 13:16:36', '2018-06-27 11:15:31', 'img/user_icon.png');
+(1, 'Christian', 'relyce4@gmail.com', '$2y$10$IR0wpHJIB5mJYPKDqk2MCOAU9t9G6RLNHrWPLJQtO5canw17Y5yZq', 'kNZck7SRpr3BtwAuT6pCn1RgmhbN5SHjfwRYl3MaxJbzdApSaB4h1osRXvs0', '2018-07-02 08:23:34', '2018-06-23 12:24:00', 'img/user_icon.png'),
+(2, 'Giuseppe', 'giuseppe@gmail.com', '$2y$10$p1N/kCnmN8s0QDcyZE0P4eou1j8lpoeQTV134PlGz8CP83G3FfeHq', 'raAdmMeI3itjWXjuvQ3vrt0l9PtCAvOODvYQL8OKrCc5lWAW5ZkBls7fmqkh', '2018-07-02 08:23:17', '2018-06-27 11:15:31', 'img/user_icon.png');
 
 --
--- Indici per le tabelle scaricate
+-- Indexes for dumped tables
 --
 
 --
--- Indici per le tabelle `migrations`
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `category` (`category`);
+
+--
+-- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indici per le tabelle `notes`
+-- Indexes for table `notes`
 --
 ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `file_name` (`file_name`,`user_id`);
+
+--
+-- Indexes for table `note_tag`
+--
+ALTER TABLE `note_tag`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indici per le tabelle `password_resets`
+-- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`),
   ADD KEY `password_resets_token_index` (`token`);
 
 --
--- Indici per le tabelle `users`
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -140,21 +207,36 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `name` (`name`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT per la tabella `migrations`
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT per la tabella `notes`
+-- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
--- AUTO_INCREMENT per la tabella `users`
+-- AUTO_INCREMENT for table `note_tag`
+--
+ALTER TABLE `note_tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
